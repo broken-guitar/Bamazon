db84f7d1ce99b063af98c0b393502e27cf8cf910
David Flores <drfloresj@gmail.com>
2020-04-25 16:18:58 -0700

    minimum requirement met

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,7 @@
 // load node module dependencies 
 const inquirer = require("inquirer");
 const mysql = require("mysql");
+const consoleTable = require("console.table");
 
 const bamazonDb = mysql.createConnection({
     host: "localhost",
@@ -10,29 +11,39 @@ const bamazonDb = mysql.createConnection({
     database: "bamazon"
 });
 
-inputProductId = [{
-    name: "productId",
-    message: "Enter a product Id",
-    default: 1,
-    validate: val => {
-       return isEmpty(val) ? "Please enter a product Id number" :  true
+var productIds = [];
+
+inputProduct = [
+    {
+        type: "number",
+        name: "productId",
+        message: "Enter the id of a product to buy:",
+        default: 1,
+        validate: val => {
+           // check that input is a number and is one of available product Ids
+            if (!isNaN(val) && productIds.indexOf(val) >= 0) {
+               return true;
+            } else {
+               return "Please enter the id number of a product in our catalog";
+            }
+        }
     },
-    filter: val => { return val.trim(); } 
- }];
+    {
+        type: "number",
+        name: "productQty",
+        message: "Enter quantity of the product to buy:",
+        default: 1,
+        validate: val => {
+            if (!isNaN(val)) {
+                return true;
+            } else {
+                return "Please enter the id number of a product in our catalog";
+            }
+        }
+    }
+];
 
-inputProductQty = [{
-    name: "productQty",
-    message: "enter product quantity",
-    default: 1,
-    validate: val => {
-        return isEmpty(val) ? "Please enter the how many you would like to buy" :  true
-     },
-     filter: val => { return val.trim(); } 
-
-}];
-
-
-// Start
+// start
 
 bamazonDb.connect(err => {
     if (err) {
@@ -42,8 +53,65 @@ bamazonDb.connect(err => {
     console.log("connected as id " + bamazonDb.threadId);
 });
 
+// show list of products to user
 bamazonDb.query("SELECT * FROM products;", (err, res) => {
+    // * note: res = array of rows as objects
     if (err) throw err;
-    console.log(res);
+    
+    // output the products table to console
+    console.table(res);
+    
+    // store all product ids in a global array (once) for validation on user input later
+    if (productIds.length <= 0) productIds = res.map(p => p.id) 
+    
+    // run function to prompt user for product input
+    saleProduct();
 });
 
+function saleProduct() {
+    inquirer
+    .prompt(inputProduct)
+    .then((answers) => {
+        let inputId = answers.productId;
+        let inputQuantity = answers.productQty;
+        bamazonDb.query("SELECT * FROM products WHERE ?", [{id: inputId}], (err, res) => {
+            if (err) throw err;
+            let resQuantity = res[0].stock_quantity;
+            let resPrice = res[0].price;
+            
+            // determine if there's enough product in stock to make the order
+            if (inputQuantity > res[0].stock_quantity) {
+                console.log("There is not enough product in stock! Your order has not been placed.\nPlease revise our order.")
+                return saleProduct(); // return to product selection prompt
+            } else {
+                let newQuantity = resQuantity - inputQuantity;
+                // update the database with the new product quantity
+                bamazonDb.query("UPDATE products SET ? WHERE ?",
+                    [{stock_quantity: newQuantity}, {id: res[0].id}], (err, result) => {
+                        if (err) throw err;
+                    }
+                );
+                // show total cost to user
+                console.log("\nYour order total is $" + answers.productQty * res[0].price);
+                inquirer
+                    .prompt({type: "confirm", name: "keepShopping", message: "Would you like to place another order?", default: "true"})
+                    .then(answers => {
+                        if (answers.keepShopping) {
+                            saleProduct();
+                        } else {
+                            console.log("\nThank you for visiting. Have a nice day!");
+                            process.exit();
+                        }
+                    });
+            }
+        });
+    });
+};
+
+
+// ### FUNCTIONS
+
+// helper - check if string is empty
+function isEmpty(string) {
+    return string.match(/^\s*$/);
+ }
